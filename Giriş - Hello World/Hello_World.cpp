afb4771c7e1557812be013068d6f0cc5f4ad3bc4
Yasin TÜRKMENOĞLU <54210105+Yasintrkmn@users.noreply.github.com>
2021-07-16 11:12:32 +0300

    Update Hello_World.cpp

--- Giriş - Hello World/Hello_World.cpp
+++ Giriş - Hello World/Hello_World.cpp
@@ -3,6 +3,6 @@ using namespace std;
 
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hello World 2 !!!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
