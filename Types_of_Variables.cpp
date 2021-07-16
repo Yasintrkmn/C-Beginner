3304ae78df1ef4f762f6fc02022997a05fdc4237
Yasin TÜRKMENOĞLU <54210105+Yasintrkmn@users.noreply.github.com>
2021-07-16 11:16:02 +0300

    Create Types_of_Variables.cpp

---
+++ Types_of_Variables.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	int a = 32;
+	float b = 3.52;
+	double c = 4.546;
+	char d = 'A'; 
+	char e = 65; // Ascii tablosundaki karakter döndürülür.
+	bool f = true;
+
+	cout << "a :" << a << endl;
+	cout << "b :" << b << endl;
+	cout << "c :" << c << endl;
+	cout << "d :" << d << endl;
+	cout << "e :" << e << endl;
+	cout << "f :" << f << endl;
+
+	sum = a + b + c;
+
+	cout << "Addition: " << sum << endl;
+
+
+	return 0;
+
+}
