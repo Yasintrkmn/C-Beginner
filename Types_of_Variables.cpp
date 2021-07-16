52c31d11e119335b3cfca9f96e3b3a9e8bc7da99
Yasin TÜRKMENOĞLU <54210105+Yasintrkmn@users.noreply.github.com>
2021-07-16 11:35:31 +0300

    Update Types_of_Variables.cpp

--- Types_of_Variables.cpp
+++ Types_of_Variables.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
-
 using namespace std;
+
+/*
+int: integer veri tipi bellekte 4 baytlık yer kaplar. Tam sayılar için kullanılır.
+float: float veri tipi bellekte 4 baytlık yer kaplar. Ondalıklı sayılar için kullanılır.
+double: double veri tipi bellekte 8 baytlık yer kaplar. Virgülden sonra daha çok değer var ise float yerine double kullanılır.
+char: char veri tipi bellekte 1 baytlık yer kaplar. Krakter verileri için kullanılır.
+*/
 
 int main(){
 
@@ -18,7 +24,8 @@ int main(){
 	cout << "e :" << e << endl;
 	cout << "f :" << f << endl;
 
-	sum = a + b + c;
+	// Toplama operatörü ile int, double, float gibi veri tipindeki değerleri toplayabiliriz.
+	double sum = a + b + c;
 
 	cout << "Addition: " << sum << endl;
 
